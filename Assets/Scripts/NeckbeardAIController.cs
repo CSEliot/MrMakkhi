39b319cbf37e5b484808f21b4ddcea472c56d36c
U-Akragas\agrub_000 <agrub_000@Akragas.(none)>
2014-07-25 23:19:08 -0400

    Added the neckbeard prefab.

---
+++ Assets/Scripts/NeckbeardAIController.cs
@@ -0,0 +1,57 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class NeckbeardAIController : MonoBehaviour 
+{
+	
+	public enum BehaviorType
+	{
+		STANDING,
+		PACING,
+		AVOIDING
+	}
+	
+	public BehaviorType type;
+	public Vector2 startPosition;
+	public Vector2 endPosition;
+	
+	//private 
+	
+	// Use this for initialization
+	void Start () 
+    {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () 
+    {
+	    switch (type)
+		{
+		case BehaviorType.STANDING:
+			StandingBehavior();
+			break;
+		case BehaviorType.PACING:
+			PacingBehavior();
+			break;
+		case BehaviorType.AVOIDING:
+			AvoidingBehavior();
+			break;
+		}
+	}
+	
+	private void StandingBehavior()
+	{
+			
+	}
+	
+	private void PacingBehavior()
+	{
+		
+	}
+	
+	private void AvoidingBehavior()
+	{
+		
+	}
+}
