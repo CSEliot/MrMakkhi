1a1fe44b6da8d62a7804e7cacae5167ada55f25b
U-Akragas\agrub_000 <agrub_000@Akragas.(none)>
2014-07-26 01:11:12 -0400

    Neckbeard Behavior Shit

--- Assets/Scripts/NeckbeardAIController.cs
+++ Assets/Scripts/NeckbeardAIController.cs
@@ -12,15 +12,16 @@ public class NeckbeardAIController : MonoBehaviour
 	}
 	
 	public BehaviorType type;
-	public Vector2 startPosition;
-	public Vector2 endPosition;
+	public Vector3 moveTo;
+	public Vector3 moveFrom;
+	private Vector3 temp;
 	
-	//private 
+	private TweenComponent tweener;
 	
 	// Use this for initialization
 	void Start () 
     {
-		
+		tweener = this.transform.GetComponent<TweenComponent>();
 	}
 	
 	// Update is called once per frame
@@ -47,7 +48,19 @@ public class NeckbeardAIController : MonoBehaviour
 	
 	private void PacingBehavior()
 	{
-		
+		if (this.transform.position.Equals(this.moveFrom))
+		{
+			tweener.StartMovement(moveTo, 10f);
+			temp.x = moveFrom.x;
+			temp.y = moveFrom.y;
+			temp.z = moveFrom.z;
+			moveFrom.x = moveTo.x;
+			moveFrom.y = moveTo.y;
+			moveFrom.z = moveTo.z;
+			moveTo.x = temp.x;
+			moveTo.y = temp.y;
+			moveTo.z = temp.z;
+		}
 	}
 	
 	private void AvoidingBehavior()
