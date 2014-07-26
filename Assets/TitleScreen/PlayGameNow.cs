f0a44954b8afc7cc34aa259c64266b0f13cc519e
Rooney Koplos <brick@fightclubgames.net>
2014-07-26 17:12:25 -0400

    Adding functional title screen

---
+++ Assets/TitleScreen/PlayGameNow.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayGameNow : MonoBehaviour {
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyUp ("space")) Application.LoadLevel ("TheActualJungle");
+	}
+}
