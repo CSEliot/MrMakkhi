34e304190bd7cf8542db8aace632cfa0a134b242
Rooney Koplos <brick@fightclubgames.net>
2014-07-26 20:49:58 -0400

    Added a lot of trees. A lot.

--- Assets/Scripts/NeckbeardSpawner.cs
+++ Assets/Scripts/NeckbeardSpawner.cs
@@ -7,12 +7,18 @@ public class NeckbeardSpawner : MonoBehaviour {
 	private float time = 0;
 	public GameObject neckbeard;
 
+	private bool firstspawned = false;
+
 	// Update is called once per frame
 	void Update () {
+		if (!firstspawned) {
+						Instantiate (neckbeard, transform.position, transform.rotation);
+						firstspawned = true;
+				}
 		time += Time.deltaTime;
 		if(time > timerTime){
 			time = 0;
-			//Instantiate(neckbeard, transform.position, transform.rotation);
+			Instantiate(neckbeard, transform.position, transform.rotation);
 		}
 	}
 }
--- Assets/TitleScreen/PlayGameNow.cs
+++ Assets/TitleScreen/PlayGameNow.cs
@@ -5,6 +5,6 @@ public class PlayGameNow : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		if (Input.GetKeyUp ("space")) Application.LoadLevel ("TheActualJungle");
+		if (Input.GetKeyUp ("space")) Application.LoadLevel ("TheActualJungleLevel");
 	}
 }
