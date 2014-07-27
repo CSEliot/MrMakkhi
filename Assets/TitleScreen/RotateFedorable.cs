b1a86c1835581c6675dddbcfe458f622b09d1edb
Rooney Koplos <brick@fightclubgames.net>
2014-07-26 21:35:04 -0400

    Added fedoras to the title screen and a bunch of other stuff

---
+++ Assets/TitleScreen/RotateFedorable.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RotateFedorable : MonoBehaviour {
+
+	public float speed;
+	
+	// Update is called once per frame
+	void Update () {
+		transform.Rotate (new Vector3(0,speed,0));
+	}
+}
